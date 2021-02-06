5a76fe41b376887dfc98f34ed98fdef8d1d83658
Morgana Bagatini <morganabagatini@gmail.com>
2021-02-06 18:23:16 -0300

    Validação do login

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
+import React, { useRef, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
+import getValidationErrors from '../../utils/getValidationErrors';
 import { Cointainer, Content, Background } from './styles';
 import logo from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
+interface FormDTO {
+  email: string;
+  password: string;
+}
+
 const Signin: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: FormDTO): Promise<void> => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('Campo obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().required('Campo obrigatório'),
+      });
+
+      await schema.validate(data, { abortEarly: false });
+    } catch (error) {
+      const errors = getValidationErrors(error);
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+
   return (
     <Cointainer>
       <Content>
         <img src={logo} alt="GoBarber" />
 
-        <form>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu logon</h1>
 
-          <Input name="email" icon={FiMail} type="text" placeholder="E-mail" />
+          <Input icon={FiMail} name="email" type="text" placeholder="E-mail" />
           <Input
-            name="Password"
             icon={FiLock}
+            name="password"
             type="password"
             placeholder="Senha"
           />
@@ -28,7 +57,7 @@ const Signin: React.FC = () => {
           <Button type="submit">Entrar</Button>
 
           <a href="/">Esqueci minha senha</a>
-        </form>
+        </Form>
 
         <Link to="/signup">
           <FiLogIn /> Criar conta
