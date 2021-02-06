de99901858b1cecb936821f61599c2feae655637
Morgana Bagatini <morganabagatini@gmail.com>
2021-02-05 23:08:49 -0300

    Componentes

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
 import { Cointainer, Content, Background } from './styles';
-
 import logo from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const Signin: React.FC = () => {
   return (
@@ -15,10 +16,15 @@ const Signin: React.FC = () => {
         <form>
           <h1>Faça seu logon</h1>
 
-          <input type="text" placeholder="E-mail" />
-          <input type="password" placeholder="Senha" />
+          <Input name="email" icon={FiMail} type="text" placeholder="E-mail" />
+          <Input
+            name="Password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
 
-          <button type="submit">Entrar</button>
+          <Button type="submit">Entrar</Button>
 
           <a href="/">Esqueci minha senha</a>
         </form>
