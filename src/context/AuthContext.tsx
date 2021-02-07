7caece9dbcfcc890c41c2d9f0e27073c3f27f0c8
Morgana Bagatini <morganabagatini@gmail.com>
2021-02-06 23:48:50 -0300

    Context de autenticação (login)

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,31 +1,52 @@
-import React, { createContext, useCallback } from 'react';
+import React, { createContext, useCallback, useState } from 'react';
 
 import api from '../services/api';
-
-interface AuthContextDTO {
-  name: string;
-  handleSignIn(credentials: SignInCredentials): Promise<void>;
-}
 
 interface SignInCredentials {
   email: string;
   password: string;
 }
 
+interface AuthState {
+  token: string;
+  user: object;
+}
+
+interface AuthContextDTO {
+  handleSignIn(credentials: SignInCredentials): Promise<void>;
+  user: object;
+}
+
 export const AuthContext = createContext<AuthContextDTO>({} as AuthContextDTO);
 
 export const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@GoBarber:token');
+    const user = localStorage.getItem('@GoBarber:user');
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+
+    return {} as AuthState;
+  });
+
   const handleSignIn = useCallback(async ({ email, password }) => {
     const response = await api.post('/sessions', {
       email,
       password,
     });
 
-    console.log(response.data);
+    const { token, user } = response.data;
+
+    localStorage.setItem('@GoBarber:token', token);
+    localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+    setData({ token, user });
   }, []);
 
   return (
-    <AuthContext.Provider value={{ name: '', handleSignIn }}>
+    <AuthContext.Provider value={{ user: data.user, handleSignIn }}>
       {children}
     </AuthContext.Provider>
   );
