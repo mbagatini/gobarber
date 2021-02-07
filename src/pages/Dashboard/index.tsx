4cf92b6ec6b2cb52c967bbf03c9d2e4aa300c83a
Morgana Bagatini <morganabagatini@gmail.com>
2021-02-07 14:06:25 -0300

    Valida se o usuário esta autenticado ao navegar pelas rotas

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+  return <h3>estou no dash</h3>;
+};
+
+export default Dashboard;
---
+++ src/routes/Route.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  Route as ReactDOMRoute,
+  RouteProps as ReactDOMRouteProps,
+  Redirect,
+} from 'react-router-dom';
+
+import { useAuth } from '../hooks/auth';
+
+/**
+ * Esse arquivo faz a verificação das rotas autenticadas,
+ * se o usuário não está autenticado, redireciona para o login
+ * se está, continua para o dashboard
+ */
+
+interface RouteProps extends ReactDOMRouteProps {
+  isPrivate?: boolean;
+  component: React.ComponentType;
+}
+
+const Route: React.FC<RouteProps> = ({
+  isPrivate = false,
+  component: Component,
+  ...rest
+}) => {
+  const { user } = useAuth();
+
+  return (
+    <ReactDOMRoute
+      {...rest}
+      render={({ location }) => {
+        return isPrivate === !!user ? (
+          <Component />
+        ) : (
+          <Redirect
+            to={{
+              pathname: isPrivate ? '/' : '/dashboard',
+              state: { from: location },
+            }}
+          />
+        );
+      }}
+    />
+  );
+};
+
+export default Route;
